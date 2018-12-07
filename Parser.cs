64eb92d845d983bc3c357cdbdce6ad0ab2e2ad76
Yuki <yuki08281993@gmail.com>
2018-12-07 16:07:17 +0900

    add moc of parser

---
+++ Parser.cs
@@ -0,0 +1,12 @@
+namespace expression {
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+
+    class Parser {
+
+        public Expr Parse(string stringOfExpression) {
+            throw new NotImplementedException(); //TODO implement it.
+        }
+    }
+}
