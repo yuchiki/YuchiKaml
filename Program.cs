09d809ee53c3857478137f45ab2d1332404dd64e
Yuki <yuki08281993@gmail.com>
2018-12-18 16:34:16 +0900

    add file mode

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 ﻿namespace expression {
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System;
+    using Sprache;
 
     class Program {
-        static void Main() => TestSuits.Test();
+        static void Main(string[] arg) {
+            if (arg.Length == 0) {
+                Console.WriteLine("No file specified.");
+                Console.WriteLine("Tests are run.");
+                TestSuits.Test();
+                return;
+            }
+
+            var text = File.ReadAllText(arg[0]);
+            var program = ExprParser.MainParser.Parse(text);
+            var value = program.Calculate();
+            Console.WriteLine($"return value is: {value}");
+        }
     }
 }
